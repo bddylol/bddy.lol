264d36b72380850d87ddaa3e8763e9e9da4436d1
Avery <git@bddy.lol>
2024-05-14 17:27:49 -0400

    something

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 
 const app = express();
+// get port from environment variable or use 3000 as default
+const port = process.env.PORT || 3000;
+const production = process.env.NODE_ENV === "production";
 
 app.set("view engine", "pug");
 
@@ -8,6 +11,10 @@ app.get("/", (req, res) => {
   res.render("index", { example: "Express + Pug" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+  console.log(
+    `Server is running at ${
+      production ? "https://bddy.lol" : `http://localhost:${port}`
+    }`
+  );
 });
